246d474d14028033226f458c5a62e3c195f4096a
Rok Kanduti <rok.kanduti@gmail.com>
2019-03-10 18:34:41 +0100

    Logo updated.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,7 +7,7 @@
             <v-card class="elevation-1 pa-3">
               <v-card-text>
                 <div class="layout column align-center">
-                  <img src="/static/m.png" alt="Vue Material Admin" width="120" height="120">
+                  <img src="/static/m.png" alt="Vue Material Admin" width="220" height="200">
                   <h1 class="flex my-4 primary--text">TeamBan</h1>
                 </div>                
                 <v-form>
